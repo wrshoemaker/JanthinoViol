5ff80f0718d5380c38bda6d8e1242a35b9105bd9
Jared Brewer <jaredbrewer@me.com>
2015-07-23 13:41:26 -0400

    Reasonable pausing point to gather feedback.

--- scripts/StarvationColony_07222015_WJB.R
+++ scripts/StarvationColony_07222015_WJB.R
@@ -6,34 +6,22 @@
 # Run a regression and make a line plot.
 
 tencol <- read.csv("StarvationColony_TenDay_07232015_WJB.csv", header = T)
-onecol <- read.csv("StarvationColony_OneDay_07222015_WJB.csv", header = T)
+onecol <- read.csv("StarvationColony_OneDay_07232015_WJB.csv", header = T)
 
 # Look at relative abundance and graph separately.
 # Also think about doing ratios instead of absolute numbers.
 
 plot(tencol$Day, tencol$Ratio, col='purple')
 # So these look a lot better, but I'm confused about how to best separate the white and purple ones. So close.
-abline(lm(Day~Purple, data=tencol, col='purple'))
-abline(lm(Day~White, data=tencol, col='black'))
+abline(lm(Day~Ratio, data=tencol))
 
 # Run a regression and look for differences, one at a time.
-tencolP.reg <- lm(Day~Purple, data=tencol)
-summary(tencolP.reg)
+tencol.reg <- lm(Day~Ratio, data=tencol)
+summary(tencol.reg)
 # Significance! 
 
-tencolW.reg <- lm(Day~White, data=tencol)
-summary(tencolW.reg)
-# More significance! 
+plot(onecol$Day, onecol$Ratio, col='purple')
+abline(lm(Day~Ratio, data=onecol))
 
-plot(onecol$Day, onecol$Purple, col='purple')
-points(onecol$Day, onecol$White, col='black')
-abline(lm(Day~Purple, data=onecol, col='purple'))
-abline(lm(Day~White, data=onecol, col='black'))
-
-onecolP.reg <- lm(Day~Purple, data=onecol)
-summary(onecolP.reg)
-# Not signficant.
-
-onecolW.reg <- lm(Day~White, data=onecol)
-summary(onecolW.reg)
-# Significant. 
+onecol.reg <- lm(Day~Ratio, data=onecol)
+summary(onecol.reg)
